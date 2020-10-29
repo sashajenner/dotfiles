9ea7ed369cf61a58f1bd4815775869b21de6f3e0
Sasha Jenner <jenner.sasha@gmail.com>
2020-10-29 13:22:43 +1100

    Updates

--- .local/src/dwmblocks/blocks.h
+++ .local/src/dwmblocks/blocks.h
@@ -5,17 +5,18 @@ static const Block blocks[] = {
 
     // Packages to update
 	//{"",      "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-    
+
 	//{"",        "shownightmode",0,		            2}, // sig 2
-	{"",        "showtime",	    30,		            0}, // 30 sec
+    {"",        "showvpn-wrapper",  0,                  2}, // sig 2
+	{"",        "showtime",	        30,		            0}, // 30 sec
 	{"",        "showvolume -q",	0,		            1}, // sig 1
 	//{"",        "showdate",	    300,		        0}, // 5 min
 	//{"",        "showwttr",	    3600,		        0}, // 1 hr
 	//{"",        "showsurf",	    10800,		        0}, // 3 hr
-	{"",        "showcpus",	    10,		            0}, // 10 sec
+	{"",        "showcpus",	        10,		            0}, // 10 sec
 	//{"",        "showwifi",	    10,		            0}, // 10 sec
 	//{"",        "showvolume",	0,		            1}, // sig 1
-	{"",        "showbattery",  10,		            0}, // 10 sec
+	{"",        "showbattery",      10,		            0}, // 10 sec
 
 };
 
